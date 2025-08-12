0c155f631d9897c5bc4d31b0f9dae09c81dc26c1
CupidVybz <omondiokothdan@gmail.com>
2025-08-12 23:35:43 +0300

    Python functions

--- Assignment.py
+++ Assignment.py
@@ -1,1 +1,16 @@
+Is_running = True
+while Is_running:
 
+    try:
+        def create_discount(price,discount_percent):
+            if discount_percent >= 20:
+                return price - (price *discount_percent/100)
+            else:
+                return price
+            
+        price = int(input("Enter the original price of the item: "))
+        discount_percent = int(input("Enter the discount Percentage: "))
+
+        print(create_discount(price=price, discount_percent=discount_percent))
+    except ValueError:
+        print("Please Enter only Numbers IDIOT!")
